35a58a42e4e2abb6ad8aca6f07593197318ccfe1
SAdnen <saadaoui.adnen@gmail.com>
2016-04-11 09:43:03 +0100

    first commit

---
+++ build/CMakeFiles/velDD2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/velDD2.dir/velDD2.cc.o"
+  "velDD2.pdb"
+  "velDD2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/velDD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
