dc97bfbe34ebd3d6f58537efa35e2982a1ae3930
SAdnen <saadaoui.adnen@gmail.com>
2016-04-11 09:47:41 +0100

    first commit

---
+++ arm_plugin/Release/CMakeFiles/arm_plugin.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/adnen/workspace/arm_plugin/arm_plugin.cc" "/home/adnen/workspace/arm_plugin/Release/CMakeFiles/arm_plugin.dir/arm_plugin.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gazebo-2.2"
+  "/usr/include/sdformat-4.0"
+  "/usr/include/ignition/math2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ arm_plugin/Release/CMakeFiles/arm_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/arm_plugin.dir/arm_plugin.cc.o"
+  "libarm_plugin.pdb"
+  "libarm_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/arm_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
